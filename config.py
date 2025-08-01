1035b4f0fd08a2440a8908f94df6d77c75c87abb
Bradjones77 <bradleyjones77@hotmail.com>
2025-08-01 20:25:55 +1200

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ BINANCE_API_SECRET = "your_binance_api_secret_here"
 COINBASE_API_KEY = "your_coinbase_api_key_here"
 COINBASE_API_SECRET = "your_coinbase_api_secret_here"
 
-TELEGRAM_BOT_TOKEN = "your_telegram_bot_token_here"
+TELEGRAM_BOT_TOKEN = "8463095004:AAEcD4I00er3kYDVZTgTROP9T1e_XkJapDU"
