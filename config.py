e2658c69637385119cbf98915c1d2419a8c67797
Bradjones77 <bradleyjones77@hotmail.com>
2025-08-01 20:14:28 +1200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+BINANCE_API_KEY = "your_binance_api_key_here"
+BINANCE_API_SECRET = "your_binance_api_secret_here"
+
+COINBASE_API_KEY = "your_coinbase_api_key_here"
+COINBASE_API_SECRET = "your_coinbase_api_secret_here"
+
+TELEGRAM_BOT_TOKEN = "your_telegram_bot_token_here"
